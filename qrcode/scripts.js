7c4c1364c4a27d100ca568fffbac3a395844fc32
sabbysabess <124396604+sabbysabess@users.noreply.github.com>
2023-08-18 16:57:57 -0300

    Add files via upload

---
+++ qrcode/scripts.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = container.querySelector("#qr-form input");
+const qrCodeImg = container.querySelector("#qr-code img");
+
+function generateQrCode() {
+  let qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerText = "Gerando código...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateQrCode();
+  }
+});
+
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
